260b144e1a746629436f07267e7e5c4739ab3045
Aldo Lisi <aldo.lisi@elitestudio.io>
2022-07-14 17:34:10 +0200

    udpates

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -18,7 +18,7 @@ class ApiController extends AbstractController
             throw new UserNotFoundException('API Key is not correct');
         }
 
-        $response = array();
+        $response = [];
 
         $lines = array_filter(preg_split('/\n|\r\n?/', $request->getContent()));
         $headers = array_shift($lines);
@@ -27,13 +27,44 @@ class ApiController extends AbstractController
         foreach ($lines as $line) {
             $array[] = array_combine(explode(',', $headers), explode(',', $line));
         }
-
-        foreach ($array as $arr){
-            $key = array_search('C Suit', $arr);
-            if($key){
-                $response['C Suit'] = array($arr);
-                dump($key);
+//        $newHeader = array_shift($array);
+//        $response[$newHeader['Team']][] = [
+//            'team' => $newHeader['Team'],
+//            'parentTeam' => $newHeader['parent_team'],
+//            'managerName' => $newHeader['manager_name'],
+//            'businessUnit' => $newHeader['business_unit'],
+//        ];
+        for ($i = 0; $i<count($array); $i++){
+            for($j = 0; $j<count($array); $j++){
+                if($array[$i]['Team'] === $array[$j]['parent_team']){
+                    $response[$array[$i]['Team']][] = $array[$j];
+//                    dump($array);
+                }
             }
+        }
+        dump($response);
+        die;
+        foreach ($array as $arr){
+//            dump($arr);
+            $team = $arr['Team'];
+//            dump($team);
+//            if($arr['parent_team'] === $newHeader['Team'])
+//                $response[$newHeader['Team']]['teams'][] = $arr;
+            foreach ($arr as $a){
+                if($a === $team){
+                    dump($a);
+                }
+            }
+            die;
+//            dump($response);
+
+//            $key = array_search('C Suit', $arr);
+//            if($key){
+//                $response['C Suit'] = array(
+//                    'teamName' =>
+//                    $arr);
+//                dump($key);
+//            }
 //            dump($key);
 
 //            foreach ($arr as $a){
@@ -41,7 +72,7 @@ class ApiController extends AbstractController
 
 //            }
         }
-//        die;
+        die;
         dd($response);
     }
 }
